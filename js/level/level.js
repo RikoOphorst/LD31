cb45cc6857e4504bbcda1795ca5de75ec7f6926a
Daniël Konings <konings_304@hotmail.com>
2014-12-06 06:47:34 +0100

    Hot reloading fix in Snuffbox

--- js/level/level.js
+++ js/level/level.js
@@ -3,16 +3,16 @@ require("js/level/player");
 var Level = function()
 {
 	this._surface = Quad2D.new();
-	//this._surface.spawn("Default");
+	this._surface.spawn("Default");
 	this._surface.setTexture("textures/level/background.png");
 	this._surface.setToTexture();
 	this._surface.setOffset(0.5, 0.5);
 
 	RenderTargets.lighting.setShader("shaders/lighting.fx");
-	this._overlay = Quad2D.new();
+	this._overlay = Widget.new();
 	this._overlay.spawn("Lighting");
-	this._overlay.setTexture("textures/level/background.png");
-	this._overlay.setSize(128,128);
+	this._overlay.setTexture("textures/level/overlay.png");
+	this._overlay.setToTexture();
 	this._overlay.setOffset(0.5, 0.5);
 	this._overlay.setTranslation(0, 0, 3);
 
