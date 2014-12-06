ee4da1bbd1b122dec71bfc099c829a8e757706be
Daniël Konings <konings_304@hotmail.com>
2014-12-06 12:54:13 +0100

    Ambient filter

--- js/level/light_overlay.js
+++ js/level/light_overlay.js
@@ -3,8 +3,9 @@ var LightOverlay = function()
 	this._overlay = Quad2D.new();
 	this._overlay.spawn("Lighting");
 	this._overlay.setSize(1280, 720);
-	this._overlay.setBlend(0,0,0);
+	this._overlay.setBlend(0.02,0,0.07);
 	this._overlay.setTranslation(0, 0, 3);
+	this._overlay.setOffset(0.5, 0.5);
 	this._timer = 0;
 
 	this.update = function(dt)
