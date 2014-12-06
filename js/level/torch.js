95ded47295cef211a6c5af04312e87c33717b1fb
Daniël Konings <konings_304@hotmail.com>
2014-12-06 12:21:06 +0100

    Torch light

--- js/level/torch.js
+++ js/level/torch.js
@@ -12,6 +12,14 @@ var Torch = function(x,y)
 	this.setScale(0.5,0.5);
 	this.setOffset(0.5,1);
 
+	this._light = Quad2D.new();
+	this._light.setTexture("textures/level/torch/torch_light.png");
+	this._light.setToTexture();
+	this._light.spawn("Lighting");
+	this._light.setTranslation(x,y-170,360+y);
+	this._light.setOffset(0.5, 0.5);
+	this._light.setBlend(1,0.9,0.4);
+
 	var frames = [];
 
 	for (var i = 0; i < 10; ++i)
@@ -28,9 +36,23 @@ var Torch = function(x,y)
 	this._animation.setSpeed(16);
 	this._animation.start();
 	this._animation.setLoop(true);
+	this._timer = 0;
+	this._targetTimer = 0.05
 
 	this.update = function(dt)
 	{
 		this._animation.update(dt);
+
+		this._timer += dt;
+
+		if (this._timer >= this._targetTimer)
+		{
+			this._timer  = 0;
+			this._targetTime = Math.randomRange(0, 0.3);
+			var scale = Math.randomRange(2.5,2.55);
+			this._light.setScale(scale,scale);
+			var alpha = Math.randomRange(0.9,1);
+			this._light.setAlpha(alpha);
+		}
 	}
 }
