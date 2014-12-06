12a8811dd6b753dc84d0e36275f70db317a17713
Riko Ophorst <riko_ophorst@hotmail.com>
2014-12-06 08:57:40 +0100

    Storage class

--- js/level/level.js
+++ js/level/level.js
@@ -1,5 +1,6 @@
 require("js/level/player");
 require("js/level/hud");
+require("js/level/storage");
 
 var Level = function()
 {
@@ -19,6 +20,8 @@ var Level = function()
 
 	this._player = new Player();
 	this._hud = new HUD();
+	this._stash = new Storage(20);
+	this._inventory = new Storage(50);
 
 	this.update = function(dt)
 	{
