6b0024de084460bb5abbc344611052f35f0f6c24
Riko Ophorst <riko_ophorst@hotmail.com>
2014-12-06 20:59:35 +0100

    Hurdur

--- js/level/hud.js
+++ js/level/hud.js
@@ -1,42 +1,78 @@
 var HUD = function () 
 {
-    this._fireStones = Text.new();
-    this._fireStones.setTranslation(-220, 310, 0);
+    this._background = Widget.new();
+    this._background.setTexture("textures/ui/hud.png");
+    this._background.setOffset(0.5, 0.5);
+    this._background.setSize(1014,140);
+    this._background.setTranslation(0, 290, 0);
+    this._background.spawn("UI");
+
+    this._fireStones = Text.new(this._background);
+    this._fireStones.setTranslation(160, 20, 1);
+    this._fireStones.setOffset(0.5, 0.5);
     this._fireStones.setText("");
     this._fireStones.spawn("UI");
 
-    this._oil = Text.new();
-    this._oil.setTranslation(-70, 310, 0);
+    this._oil = Text.new(this._background);
+    this._oil.setTranslation(-460, 20, 1);
+    this._oil.setOffset(0.5, 0.5);
     this._oil.setText("");
     this._oil.spawn("UI");
 
-    this._wood = Text.new();
-    this._wood.setTranslation(25, 310, 0);
+    this._health = Text.new(this._background);
+    this._health.setTranslation(460, 20, 1);
+    this._health.setOffset(0.5, 0.5);
+    this._health.setText("[size=23]Health[/size]");
+    this._health.spawn("UI");
+
+    this._wood = Text.new(this._background);
+    this._wood.setTranslation(-160, 20, 1);
+    this._wood.setOffset(0.5, 0.5);
     this._wood.setText("");
     this._wood.spawn("UI");
 
-    this._seeds = Text.new();
-    this._seeds.setTranslation(140, 310, 0);
+    this._seeds = Text.new(this._background);
+    this._seeds.setTranslation(-60, 20, 1);
+    this._seeds.setOffset(0.5, 0.5);
     this._seeds.setText("");
     this._seeds.spawn("UI");
 
-    this._score = Text.new();
-    this._score.setTranslation(500, -310, 0);
+    this._score = Text.new(this._background);
+    this._score.setTranslation(0, 0, 1);
+    this._score.setOffset(0.5, 0.5);
     this._score.setText("");
-    this._score.spawn("UI");
+    //this._score.spawn("UI");
 
-    this._kills = Text.new();
-    this._kills.setTranslation(500, -280, 0);
+    this._kills = Text.new(this._background);
+    this._kills.setTranslation(0, 0, 1);
+    this._kills.setOffset(0.5, 0.5);
     this._kills.setText("");
-    this._kills.spawn("UI");
+    //this._kills.spawn("UI");
 
-    this._lumo = Text.new();
-    this._lumo.setTranslation(500, -250, 0);
+    this._lumo = Text.new(this._background);
+    this._lumo.setTranslation(60, 20, 1);
+    this._lumo.setOffset(0.5, 0.5);
     this._lumo.setText("");
     this._lumo.spawn("UI");
 
+    this._healthbar = Widget.new(this._background);
+    this._healthbar.setTexture("textures/ui/healthbar.png");
+    this._healthbar.setOffset(0, 0.5);
+    this._healthbar.setSize(164,58);
+    this._healthbar.setTranslation(240, 20, 0);
+    this._healthbar.spawn("UI");
+
+    this._oilbar = Widget.new(this._background);
+    this._oilbar.setTexture("textures/ui/healthbar.png");
+    this._oilbar.setOffset(1, 0.5);
+    this._oilbar.setSize(164,58);
+    this._oilbar.setTranslation(-240, 20, 0);
+    this._oilbar.spawn("UI");
+
+    this._background.setAlpha(0.9);
+
     this.setFireStones = function (amt) {
-        this._fireStones.setText("[size=23]Firestones: "+amt+"[/size]");
+        this._fireStones.setText("[size=23]Flint: "+amt+"[/size]");
     };
 
     this.setOil = function (amt) {
