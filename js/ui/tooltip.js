28edba9567b678e9059c53aaf971f34113b816c3
Riko Ophorst <riko_ophorst@hotmail.com>
2014-12-07 19:03:50 +0100

    Tooltip fix

--- js/ui/tooltip.js
+++ js/ui/tooltip.js
@@ -1,5 +1,5 @@
 var Tooltip = function (parent, text) {
-    this._mouseArea = MouseArea.new();
+    this._mouseArea = MouseArea.new(0, 0, 0, 0);
 
     this._text = Text.new();
     this._text.spawn("UI");
@@ -10,7 +10,7 @@ var Tooltip = function (parent, text) {
     this._background.setTranslation(0, 0, 0);
     this._background.spawn("UI");
 
-    this._mouseArea.setMetrics(x - (parent.size().w * parent.offset().x), y - (parent.size().y * parent.offset().h), parent.size().w, parent.size().h);
+    this._mouseArea.setMetrics(parent.translation().x - (parent.size().w * parent.offset().x), parent.translation().y - (parent.size().h * parent.offset().y), parent.size().w, parent.size().h);
 
     this._mouseArea.setOnEnter(function () {
         Log.fatal('HALLO');
