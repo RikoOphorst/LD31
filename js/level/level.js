f61b061a755d0225741280f7161f95f8863c3583
Riko Ophorst <riko_ophorst@hotmail.com>
2014-12-08 04:16:39 +0100

    remove shit from shit

--- js/level/level.js
+++ js/level/level.js
@@ -89,11 +89,6 @@ var Level = function(camera)
 	this._trees = [];
 	this._torches = [];
 
-	/*for (var i = 0; i < 4; ++i)
-	{
-		this._torches.push(new Torch(-640+Math.random()*1280, -150 + Math.random()*510));
-	}*/
-
 	for (var i = 0; i < 4; ++i)
 	{
 		this._trees.push(new Tree(-640+Math.random()*1280, -150 + Math.random()*510));
