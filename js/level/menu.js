732e2939ef840a8497114b8ce2de8ea76a455d70
Riko Ophorst <riko_ophorst@hotmail.com>
2014-12-09 01:07:33 +0100

    bug fix

--- js/level/menu.js
+++ js/level/menu.js
@@ -211,11 +211,6 @@ var Menu = function ()
             this._rainModifier = 1;
         }
 
-        if (Keyboard.lastPressed() !== "None")
-        {
-            StateManager.switchState(LevelState);
-        }
-
         this._thunderFlash.setAlpha(0);
         if (this.thunderTimer > 0)
         {
